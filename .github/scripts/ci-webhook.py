d6394d10af41b0ca64cad13e8c97a642c946b478
zwwhdls <weiwei.zhu@juicefs.io>
2022-12-07 14:59:44 +0800

    add webhook test in ci

---
+++ .github/scripts/ci-webhook.py
@@ -0,0 +1,49 @@
+from kubernetes import config
+
+from config import GLOBAL_MOUNTPOINT, LOG
+from test_case import (
+    test_static_delete_policy,
+    test_deployment_using_storage_rw,
+    test_deployment_using_storage_ro,
+    test_deployment_use_pv_rw,
+    test_deployment_use_pv_ro,
+    test_delete_all,
+    test_delete_one,
+    test_delete_pvc,
+    test_deployment_dynamic_patch_pv,
+    test_dynamic_delete_pod,
+    test_static_delete_pod,
+    test_deployment_static_patch_pv,
+    test_dynamic_cache_clean_upon_umount,
+    test_static_cache_clean_upon_umount,
+    test_dynamic_mount_image,
+    test_static_mount_image,
+    test_pod_resource_err,
+)
+from util import die, mount_on_host, umount, clean_juicefs_volume, deploy_secret_and_sc, tear_down, check_do_test
+
+if __name__ == "__main__":
+    if check_do_test():
+        config.load_kube_config()
+        # clear juicefs volume first.
+        LOG.info("clean juicefs volume first.")
+        mount_on_host(GLOBAL_MOUNTPOINT)
+        clean_juicefs_volume()
+        try:
+            deploy_secret_and_sc()
+            test_static_delete_policy()
+            test_deployment_using_storage_rw()
+            test_deployment_using_storage_ro()
+            test_deployment_use_pv_rw()
+            test_deployment_use_pv_ro()
+            test_deployment_dynamic_patch_pv()
+            test_deployment_static_patch_pv()
+            test_dynamic_mount_image()
+            test_static_mount_image()
+        except Exception as e:
+            die(e)
+        finally:
+            tear_down()
+            umount(GLOBAL_MOUNTPOINT)
+    else:
+        LOG.info("skip test.")
--- .github/scripts/deploy-csi-in-k8s.sh
+++ .github/scripts/deploy-csi-in-k8s.sh
@@ -3,8 +3,17 @@
 function main() {
   deployMode=$1
   echo "deployMode: " $deployMode
+  if [ $deployMode == "webhook"]; then
+    deploy_webhook
+  else
+    deploy_csi $deployMode
+  fi
+}
+
+function deploy_csi() {
+  deployMode=$1
   sudo kustomize build ${GITHUB_WORKSPACE}/deploy/kubernetes/csi-ci/$deployMode | sed -e "s@juicedata/juicefs-csi-driver.*\$@juicedata/juicefs-csi-driver:${dev_tag}@g" \
-                   -e 's@/var/lib/kubelet@/var/snap/microk8s/common/var/lib/kubelet@g' | sudo microk8s.kubectl apply -f -
+    -e 's@/var/lib/kubelet@/var/snap/microk8s/common/var/lib/kubelet@g' | sudo microk8s.kubectl apply -f -
   # Wait until the deploy finish
   timeout=0
   while true; do
@@ -25,10 +34,44 @@ function main() {
       node_pod=$(sudo microk8s.kubectl -n default get pods | grep Running | grep juicefs-csi-node | awk '{print $1}' | cut -d/ -f2)
       echo "JUICEFS_CSI_NODE_POD:" $node_pod
       echo "JUICEFS_CSI_NODE_POD=$node_pod" >>$GITHUB_ENV
-      sudo microk8s.kubectl cp default/$node_pod:/usr/local/bin/juicefs /usr/local/bin/juicefs -c juicefs-plugin && \
-          sudo chmod a+x /usr/local/bin/juicefs && juicefs -V
-      sudo microk8s.kubectl cp default/$node_pod:/usr/bin/juicefs /usr/bin/juicefs -c juicefs-plugin && \
-          sudo chmod a+x /usr/bin/juicefs && /usr/bin/juicefs version
+      sudo microk8s.kubectl cp default/$node_pod:/usr/local/bin/juicefs /usr/local/bin/juicefs -c juicefs-plugin &&
+        sudo chmod a+x /usr/local/bin/juicefs && juicefs -V
+      sudo microk8s.kubectl cp default/$node_pod:/usr/bin/juicefs /usr/bin/juicefs -c juicefs-plugin &&
+        sudo chmod a+x /usr/bin/juicefs && /usr/bin/juicefs version
+      echo "JuiceFS CSI is ready."
+      break
+    fi
+    sleep 5
+  done
+}
+
+function deploy_webhook() {
+  ds=$(sudo microk8s.kubectl get ds -n default | grep juicefs-csi-node | wc -l)
+  if [ $ds -gt 0 ]; then
+    sudo microk8s.kubectl -n default delete ds juicefs-csi-node
+  fi
+  sudo ${GITHUB_WORKSPACE}/scripts/webhook.sh print | sed -e "s@juicedata/juicefs-csi-driver.*\$@juicedata/juicefs-csi-driver:${dev_tag}@g" \
+    -e 's@/var/lib/kubelet@/var/snap/microk8s/common/var/lib/kubelet@g' | sudo microk8s.kubectl apply -f -
+  # Wait until the deploy finish
+  timeout=0
+  while true; do
+    if [ $timeout -gt 60 ]; then
+      echo "JuiceFS CSI is not ready within 5min."
+      ctrl_pod=$(sudo microk8s.kubectl -n default get pods -o name | grep juicefs-csi-controller | cut -d/ -f2)
+      sudo microk8s.kubectl -n default describe po $ctrl_pod
+      exit 1
+    fi
+    timeout=$(expr $timeout + 1)
+    echo "Wait JuiceFS CSI to be ready ..."
+    # The juicefs-csi-controller pods' containers should be all ready
+    all_count=$(sudo microk8s.kubectl -n default get pods | grep juicefs-csi-controller | wc -l)
+    count=$(sudo microk8s.kubectl -n default get pods | grep juicefs-csi | grep Running | awk '{print $2}' | tr '/' '-' | bc | grep '^0$' | wc -l)
+    if [ $count = 2 ] && [ $all_count = 2 ]; then
+      ctrl_pod=juicefs-csi-controller-0
+      sudo microk8s.kubectl cp default/$ctrl_pod:/usr/local/bin/juicefs /usr/local/bin/juicefs -c juicefs-plugin &&
+        sudo chmod a+x /usr/local/bin/juicefs && juicefs -V
+      sudo microk8s.kubectl cp default/$ctrl_pod:/usr/bin/juicefs /usr/bin/juicefs -c juicefs-plugin &&
+        sudo chmod a+x /usr/bin/juicefs && /usr/bin/juicefs version
       echo "JuiceFS CSI is ready."
       break
     fi
