08b3c0a007c42ab7c3c7278f0100ace0e86d09df
timfeirg <kkcocogogo@gmail.com>
2023-03-31 16:04:03 +0800

    preserve work before halt

--- docker/juicefs.sh
+++ docker/juicefs.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+#!/bin/sh
 
 cmdline=$(cat /proc/1/cmdline | tr -d '\0')
-any_mount_running=$(echo $cmdline | grep -ao '/mount.juicefs')
+any_mount_running=$(echo "$cmdline" | grep -ao '/mount.juicefs')
 if [[ -z "$any_mount_running" ]]
 then
   echo 'Cannot infer juicefs client from PID 1, use the following instead:'
@@ -10,7 +10,7 @@ then
   exit 0
 fi
 ee_mount_path='/sbin/mount.juicefs'
-ee_is_running=$(echo $cmdline | grep -ao "$ee_mount_path")
+ee_is_running=$(echo "$cmdline" | grep -ao "$ee_mount_path")
 if [[ ! -z "$ee_is_running" ]]
 then
   exec /usr/local/bin/juicefs-ee $@
