c5410a0a3a008005140d90b8a6846965cd7710d8
xixi <hexilee@juicedata.io>
2023-09-20 00:45:51 +0800

    add renderPodsList
    Signed-off-by: xixi <hexilee@juicedata.io>

--- dashboard-ui/main.js
+++ dashboard-ui/main.js
@@ -1,48 +1,32 @@
+const renderPodsList = (pods) => {
+    let podsList = document.getElementById("podsList");
+    podsList.innerHTML = "";
+    pods.forEach(pod => {
+        podsList.innerHTML += `<li>${pod.metadata.name}</li>`;
+    });
+}
+
 
 const listAppPods = () => {
     fetch('/api/v1/pods')
         .then(response => response.json())
-        .then(pods => {
-            let podsList = document.getElementById("podsList");
-            podsList.innerHTML = "";
-            pods.forEach(pod => {
-                podsList.innerHTML += `<li>${pod.metadata.name}</li>`;
-            });
-        });
+        .then(renderPodsList);
 }
 
 const listMountPods = () => {
     fetch('/api/v1/mountpods')
         .then(response => response.json())
-        .then(pods => {
-            let podsList = document.getElementById("podsList");
-            podsList.innerHTML = "";
-            pods.forEach(pod => {
-                podsList.innerHTML += `<li>${pod.metadata.name}</li>`;
-            });
-        });
+        .then(renderPodsList);
 }
 
 const listCSINodePods = () => {
     fetch('/api/v1/csi-nodes')
         .then(response => response.json())
-        .then(pods => {
-            let podsList = document.getElementById("podsList");
-            podsList.innerHTML = "";
-            pods.forEach(pod => {
-                podsList.innerHTML += `<li>${pod.metadata.name}</li>`;
-            });
-        });
+        .then(renderPodsList);
 }
 
 const listControllerPods = () => {
     fetch('/api/v1/controllers')
         .then(response => response.json())
-        .then(pods => {
-            let podsList = document.getElementById("podsList");
-            podsList.innerHTML = "";
-            pods.forEach(pod => {
-                podsList.innerHTML += `<li>${pod.metadata.name}</li>`;
-            });
-        });
+        .then(renderPodsList);
 }
