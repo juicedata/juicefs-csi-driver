960987c120f9d03d914ab21c0a4cc832ec8df415
xixi <hexilee@juicedata.io>
2023-09-20 14:01:25 +0800

    re-init
    Signed-off-by: xixi <hexilee@juicedata.io>

--- dashboard-ui/.eslintrc.js
+++ dashboard-ui/.eslintrc.js
@@ -1,7 +1,3 @@
 module.exports = {
-  extends: [require.resolve('@umijs/lint/dist/config/eslint')],
-  globals: {
-    page: true,
-    REACT_APP_ENV: true,
-  },
+  extends: require.resolve('@umijs/max/eslint'),
 };
