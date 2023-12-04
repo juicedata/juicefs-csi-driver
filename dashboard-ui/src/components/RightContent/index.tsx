3ad00fabd87fefa3a8a929f175b5faf5f25dc777
zwwhdls <weiwei.zhu@juicefs.io>
2023-12-04 15:46:54 +0800

    format componenets
    Signed-off-by: zwwhdls <weiwei.zhu@juicefs.io>

--- dashboard-ui/src/components/RightContent/index.tsx
+++ dashboard-ui/src/components/RightContent/index.tsx
@@ -17,24 +17,22 @@
 import { QuestionCircleOutlined } from '@ant-design/icons';
 import { SelectLang as UmiSelectLang } from '@umijs/max';
 
-export type SiderTheme = 'light' | 'dark';
-
 export const SelectLang = () => {
-  return <UmiSelectLang style={{ padding: 4 }} />;
+    return <UmiSelectLang style={{ padding: 4 }} />;
 };
 
 export const Question = () => {
-  return (
-    <div
-      style={{
-        display: 'flex',
-        height: 26,
-      }}
-      onClick={() => {
-        window.open('https://juicefs.com/docs/csi/introduction/');
-      }}
-    >
-      <QuestionCircleOutlined />
-    </div>
-  );
+    return (
+        <div
+            style={{
+                display: 'flex',
+                height: 26,
+            }}
+            onClick={() => {
+                window.open('https://juicefs.com/docs/csi/introduction/');
+            }}
+        >
+            <QuestionCircleOutlined />
+        </div>
+    );
 };
