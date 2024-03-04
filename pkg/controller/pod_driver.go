61763ce401501caa58609b05347cc128e5e8ed4e
Zzde <xuhui@juicedata.io>
2024-03-04 18:01:34 +0800

    Update pkg/controller/pod_driver.go
    Co-authored-by: timfeirg <kkcocogogo@gmail.com>

--- pkg/controller/pod_driver.go
+++ pkg/controller/pod_driver.go
@@ -82,7 +82,7 @@ func (p *PodDriver) SetMountInfo(mit mountInfoTable) {
 
 func (p *PodDriver) Run(ctx context.Context, current *corev1.Pod) error {
 	podStatus := getPodStatus(current)
-	klog.V(6).Infof("[PodDriver] start handle pod %s/%s, status: %s", current.Name, current.Namespace, podStatus)
+	klog.V(6).Infof("[PodDriver] start handle pod %s/%s, status: %s", current.Namespace, current.Name,  podStatus)
 	// check refs in mount pod annotation first, delete ref that target pod is not found
 	err := p.checkAnnotations(ctx, current)
 	if err != nil {
