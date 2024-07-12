3f1e0fe091879a3bc10db783ae9397a292cff7e9
Zzde <xuhui@juicedata.io>
2024-07-12 10:36:07 +0800

    chore: add PV subpath label to provisioned PV (#1031)
    Signed-off-by: Xuhui zhang <xuhui@juicedata.io>

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -136,6 +136,9 @@ const (
 	DefaultMountPodMemLimit   = "5Gi"
 	DefaultMountPodCpuRequest = "1000m"
 	DefaultMountPodMemRequest = "1Gi"
+
+	// pv labels
+	PVSubpathKey = "juicefs/sub-path"
 )
 
 var PodLocks [1024]sync.Mutex
