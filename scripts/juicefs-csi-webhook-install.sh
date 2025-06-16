8f96ba92e73c774179e9d52bdc0c6ffc7e796d9c
Zzde <xuhui@juicedata.io>
2025-06-16 15:39:48 +0800

    [ci skip]: release v0.28.2 (#1365)
    Signed-off-by: Xuhui zhang <xuhui@juicedata.io>

--- scripts/juicefs-csi-webhook-install.sh
+++ scripts/juicefs-csi-webhook-install.sh
@@ -519,8 +519,8 @@ spec:
             fieldRef:
               fieldPath: metadata.namespace
         - name: DASHBOARD_IMAGE
-          value: juicedata/csi-dashboard:v0.28.1
-        image: juicedata/csi-dashboard:v0.28.1
+          value: juicedata/csi-dashboard:v0.28.2
+        image: juicedata/csi-dashboard:v0.28.2
         name: dashboard
         ports:
         - containerPort: 8088
@@ -592,7 +592,7 @@ spec:
           value: /var/lib/juicefs/volume
         - name: JUICEFS_CONFIG_PATH
           value: /var/lib/juicefs/config
-        image: juicedata/juicefs-csi-driver:v0.28.1
+        image: juicedata/juicefs-csi-driver:v0.28.2
         livenessProbe:
           failureThreshold: 5
           httpGet:
@@ -1289,8 +1289,8 @@ spec:
             fieldRef:
               fieldPath: metadata.namespace
         - name: DASHBOARD_IMAGE
-          value: juicedata/csi-dashboard:v0.28.1
-        image: juicedata/csi-dashboard:v0.28.1
+          value: juicedata/csi-dashboard:v0.28.2
+        image: juicedata/csi-dashboard:v0.28.2
         name: dashboard
         ports:
         - containerPort: 8088
@@ -1362,7 +1362,7 @@ spec:
           value: /var/lib/juicefs/volume
         - name: JUICEFS_CONFIG_PATH
           value: /var/lib/juicefs/config
-        image: juicedata/juicefs-csi-driver:v0.28.1
+        image: juicedata/juicefs-csi-driver:v0.28.2
         livenessProbe:
           failureThreshold: 5
           httpGet:
