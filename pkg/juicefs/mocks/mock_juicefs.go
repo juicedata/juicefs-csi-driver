53f552105f00ee367f2a6181d911b9f0eb19a06f
Xuhui zhang <xuhui@juicedata.io>
2025-10-22 18:01:26 +0800

    compatible with ee
    Signed-off-by: Xuhui zhang <xuhui@juicedata.io>

--- pkg/juicefs/mocks/mock_juicefs.go
+++ pkg/juicefs/mocks/mock_juicefs.go
@@ -53,17 +53,17 @@ func (mr *MockInterfaceMockRecorder) AuthFs(arg0, arg1, arg2, arg3 interface{}) 
 }
 
 // CreateSnapshot mocks base method.
-func (m *MockInterface) CreateSnapshot(arg0 context.Context, arg1, arg2, arg3 string, arg4, arg5 map[string]string) error {
+func (m *MockInterface) CreateSnapshot(arg0 context.Context, arg1, arg2 string, arg3, arg4 map[string]string) error {
 	m.ctrl.T.Helper()
-	ret := m.ctrl.Call(m, "CreateSnapshot", arg0, arg1, arg2, arg3, arg4, arg5)
+	ret := m.ctrl.Call(m, "CreateSnapshot", arg0, arg1, arg2, arg3, arg4)
 	ret0, _ := ret[0].(error)
 	return ret0
 }
 
 // CreateSnapshot indicates an expected call of CreateSnapshot.
-func (mr *MockInterfaceMockRecorder) CreateSnapshot(arg0, arg1, arg2, arg3, arg4, arg5 interface{}) *gomock.Call {
+func (mr *MockInterfaceMockRecorder) CreateSnapshot(arg0, arg1, arg2, arg3, arg4 interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
-	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "CreateSnapshot", reflect.TypeOf((*MockInterface)(nil).CreateSnapshot), arg0, arg1, arg2, arg3, arg4, arg5)
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "CreateSnapshot", reflect.TypeOf((*MockInterface)(nil).CreateSnapshot), arg0, arg1, arg2, arg3, arg4)
 }
 
 // CreateTarget mocks base method.
@@ -254,17 +254,17 @@ func (mr *MockInterfaceMockRecorder) MountSensitive(arg0, arg1, arg2, arg3, arg4
 }
 
 // RestoreSnapshot mocks base method.
-func (m *MockInterface) RestoreSnapshot(arg0 context.Context, arg1, arg2, arg3 string, arg4, arg5 map[string]string) error {
+func (m *MockInterface) RestoreSnapshot(arg0 context.Context, arg1, arg2, arg3, arg4 string, arg5, arg6 map[string]string) error {
 	m.ctrl.T.Helper()
-	ret := m.ctrl.Call(m, "RestoreSnapshot", arg0, arg1, arg2, arg3, arg4, arg5)
+	ret := m.ctrl.Call(m, "RestoreSnapshot", arg0, arg1, arg2, arg3, arg4, arg5, arg6)
 	ret0, _ := ret[0].(error)
 	return ret0
 }
 
 // RestoreSnapshot indicates an expected call of RestoreSnapshot.
-func (mr *MockInterfaceMockRecorder) RestoreSnapshot(arg0, arg1, arg2, arg3, arg4, arg5 interface{}) *gomock.Call {
+func (mr *MockInterfaceMockRecorder) RestoreSnapshot(arg0, arg1, arg2, arg3, arg4, arg5, arg6 interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
-	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "RestoreSnapshot", reflect.TypeOf((*MockInterface)(nil).RestoreSnapshot), arg0, arg1, arg2, arg3, arg4, arg5)
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "RestoreSnapshot", reflect.TypeOf((*MockInterface)(nil).RestoreSnapshot), arg0, arg1, arg2, arg3, arg4, arg5, arg6)
 }
 
 // SetQuota mocks base method.
